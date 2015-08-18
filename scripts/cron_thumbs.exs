dc0ed7da47f0ddb489924467fff8caafd7d40b44
Remigiusz Jackowski <remigiusz.jackowski@gmail.com>
2015-08-18 12:38:42 +0200

    Image magick as utility container and cron script for thumb creation

---
+++ scripts/cron_thumbs.exs
@@ -0,0 +1,24 @@
+
+"""
+  CRON script
+  This script will find all images without thumbs and for each missing will
+  execute imagemagick's container to create thumb.
+"""
+imgs = File.ls!("data/files/img")
+thumbs = File.ls!("data/files/thumb")
+imgs -- thumbs
+|> Enum.each(fn file ->
+  System.cmd "docker-compose", [
+    "run", "--rm", "imagemagick",
+    "mogrify",
+    "-resize", "200x200^",
+    "-write", "/files/thumb/#{file}",
+    "/files/img/#{file}"
+  ]
+end)
+
+
+
+
+
+
